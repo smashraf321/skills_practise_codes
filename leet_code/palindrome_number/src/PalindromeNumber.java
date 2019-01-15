44c0103ebbe5fd914baf7a32676f1f313890dced
Ashraf Shaikh Mohammed <ashraf@Ashrafs-MacBook-Pro.local>
2019-01-15 10:59:05 -0600

    edited for push n pull test

--- leet_code/palindrome_number/src/PalindromeNumber.java
+++ leet_code/palindrome_number/src/PalindromeNumber.java
@@ -10,21 +10,23 @@ public class PalindromeNumber {
     }
     public static boolean isPalindrome(int x) {
         if(x < 0){
-            System.out.println(x + " < 0");
+            //System.out.println(x + " is < 0");
             return false;
         } else if (x < 10) {
-            System.out.println(x + " < 10");
+            //System.out.println(x + " is < 10");
             return true;
         } else if (x < 100) {
-            System.out.println(x + " < 100");
+            //System.out.println(x + " is < 100");
             if(x % 11 == 0) {
                 return true;
             } else {
                 return false;
             }
+        } else if (x % 10 == 0) {
+            return false;
         } else {
             int original = x;
-            System.out.println(x + " >= 100");
+            //System.out.println(x + " is >= 100 but not multiples of 10");
             int reverseNum = 0;
             while(x > 0){
                 reverseNum = reverseNum * 10 + x % 10;
